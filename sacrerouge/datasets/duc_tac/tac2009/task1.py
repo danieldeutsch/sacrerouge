b0f8023087cd3e0d4e157b965695646abeede798
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-12 22:19:52 -0400

    Adding TAC 2009

---
+++ sacrerouge/datasets/duc_tac/tac2009/task1.py
@@ -0,0 +1,173 @@
+import argparse
+import lxml.html
+import re
+import tarfile
+from collections import defaultdict
+from io import BytesIO
+from lxml import etree
+from nltk.tokenize import sent_tokenize
+
+from sacrerouge.io import JsonlWriter
+
+
+def parse_story(root):
+    headline = None
+    headline_nodes = list(root.xpath('body/doc/headline'))
+    if len(headline_nodes) > 0:
+        headline = headline_nodes[0].text_content().strip()
+
+    text = []
+    for p_node in root.xpath('body/doc/text/p'):
+        paragraph = p_node.text_content().strip()
+        paragraph = re.sub('\s+', ' ', paragraph)
+        paragraph = sent_tokenize(paragraph)
+        text.append(paragraph)
+
+    return headline, text
+
+
+def parse_other(root):
+    # Everything is combined into a single paragraph.
+    text = root.xpath('//text')[0].text_content().strip()
+    text = re.sub('\s+', ' ', text)
+    sentences = [sent_tokenize(text)]
+    return sentences
+
+
+def load_documents(documents_tar: str):
+    selectors = {}
+    documents = defaultdict(lambda: defaultdict(list))
+
+    with tarfile.open(documents_tar, 'r') as tar:
+        inner_tar_bytes = tar.extractfile('TAC2009_Summarization_Documents/UpdateSumm09_test_docs_files.tar.gz').read()
+        with tarfile.open(fileobj=BytesIO(inner_tar_bytes)) as inner_tar:
+            for member in inner_tar.getmembers():
+                if member.isfile():
+                    path = member.name.split('/')
+                    instance_id = path[1][:-1].lower()
+                    selector = path[1][-1]
+                    group = path[2][-1]
+                    filename = path[-1]
+
+                    if instance_id not in selectors:
+                        selectors[instance_id] = selector
+
+                    date = filename[8:16]
+                    year, month, day = date[:4], date[4:6], date[6:]
+                    date = f'{year}-{month}-{day}'
+
+                    html = inner_tar.extractfile(member).read().decode()
+                    tree = lxml.html.document_fromstring(html)
+
+                    # The documents come from English Gigaword. Different document
+                    # types have different structures, some of which we can parse
+                    # ("story"), but others we cannot ("other")
+                    document_type = tree.xpath('body/doc')[0].get('type')
+                    if document_type == 'story':
+                        headline, text = parse_story(tree)
+                    else:
+                        headline = None
+                        text = parse_other(tree)
+
+                    document = {
+                        'filename': filename,
+                        'document_type': document_type,
+                        'headline': headline,
+                        'date': date,
+                        'text': text
+                    }
+                    documents[instance_id][group].append(document)
+
+    return documents, selectors
+
+
+def load_summaries(eval_tar: str) -> None:
+    summaries = defaultdict(lambda: defaultdict(list))
+    with tarfile.open(eval_tar, 'r') as tar:
+        for member in tar.getmembers():
+            if member.isfile() and member.name.startswith('UpdateSumm09_eval/ROUGE/models/'):
+                path = member.name.split('/')
+                filename = path[-1]
+                parts = filename.split('.')
+                instance_id = parts[0].split('-')[0].lower()
+                group = parts[0].split('-')[1]
+                annotator = parts[4]
+
+                text = tar.extractfile(member).read().decode().strip()
+                sentences = sent_tokenize(text)
+                summary = {
+                    'annotator': annotator,
+                    'text': sentences
+                }
+                summaries[instance_id][group].append(summary)
+    return summaries
+
+
+def load_topics(file_path: str):
+    topics = {}
+    tree = etree.parse(file_path)
+    for topic_node in tree.xpath('//topic'):
+        instance_id = topic_node.get('id')[:-1].lower()
+        title = topic_node.xpath('title')[0].text.strip()
+        narrative = sent_tokenize(topic_node.xpath('narrative')[0].text.strip())
+        topics[instance_id] = {
+            'title': title,
+            'narrative': narrative
+        }
+    return topics
+
+
+def save_instances(documents, selectors, summaries, topics, output_dir: str) -> None:
+    with JsonlWriter(f'{args.output_dir}/task1.A-B.jsonl') as out_A_B:
+        with JsonlWriter(f'{args.output_dir}/task1.A.jsonl') as out_A:
+            with JsonlWriter(f'{args.output_dir}/task1.B.jsonl') as out_B:
+                for instance_id in sorted(documents.keys()):
+                    topic = topics[instance_id]
+                    selector = selectors[instance_id]
+
+                    documents_A = documents[instance_id]['A']
+                    documents_B = documents[instance_id]['B']
+
+                    summaries_A = summaries[instance_id]['A']
+                    summaries_B = summaries[instance_id]['B']
+
+                    out_A.write({
+                        'instance_id': f'{instance_id}-A',
+                        'selector': selector,
+                        'topic': topic,
+                        'documents': documents_A,
+                        'summaries': summaries_A
+                    })
+                    out_B.write({
+                        'instance_id': f'{instance_id}-B',
+                        'selector': selector,
+                        'topic': topic,
+                        'documents': documents_B,
+                        'summaries': summaries_B
+                    })
+                    out_A_B.write({
+                        'instance_id': instance_id,
+                        'selector': selector,
+                        'topic': topic,
+                        'documents_A': documents_A,
+                        'documents_B': documents_B,
+                        'summaries_A': summaries_A,
+                        'summaries_B': summaries_B
+                    })
+
+
+def main(args):
+    documents, selectors = load_documents(args.documents_tar)
+    summaries = load_summaries(args.eval_tar)
+    topics = load_topics(args.topics_file)
+    save_instances(documents, selectors, summaries, topics, args.output_dir)
+
+
+if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument('documents_tar')
+    argp.add_argument('eval_tar')
+    argp.add_argument('topics_file')
+    argp.add_argument('output_dir')
+    args = argp.parse_args()
+    main(args)
---
+++ sacrerouge/tests/datasets/duc_tac/tac2009/system_level_test.py
@@ -0,0 +1,193 @@
+import os
+import pytest
+import unittest
+
+from sacrerouge.compute_correlation import aggregate_metrics
+from sacrerouge.io import JsonlReader
+
+_metrics_A_file_path = 'datasets/duc-tac/tac2009/v1.0/task1.A.metrics.jsonl'
+_metrics_B_file_path = 'datasets/duc-tac/tac2009/v1.0/task1.B.metrics.jsonl'
+
+
+class TestTAC2009SystemLevel(unittest.TestCase):
+    @pytest.mark.skipif(not os.path.exists(_metrics_A_file_path), reason='TAC 2009-A metrics file does not exist')
+    def test_system_level_A(self):
+        summary_level_metrics = JsonlReader(_metrics_A_file_path).read()
+        system_level_metrics = aggregate_metrics(summary_level_metrics, 'summarizer_id')
+
+        # Check a few metrics to make sure they are equal to what's in the NIST files
+        # ROUGE/rouge2_A.m.avg
+        assert system_level_metrics['2']['rouge-2']['recall'] == pytest.approx(33.165, 1e-2)
+        assert system_level_metrics['34']['rouge-2']['recall'] == pytest.approx(12.163, 1e-2)
+        assert system_level_metrics['40']['rouge-2']['recall'] == pytest.approx(12.089, 1e-2)
+        assert system_level_metrics['35']['rouge-2']['recall'] == pytest.approx(10.869, 1e-2)
+        assert system_level_metrics['3']['rouge-2']['recall'] == pytest.approx(10.655, 1e-2)
+
+        # ROUGE/rouge2_A.jk.m.avg
+        assert system_level_metrics['C']['rouge-2_jk']['recall'] == pytest.approx(14.864, 1e-2)
+        assert system_level_metrics['H']['rouge-2_jk']['recall'] == pytest.approx(13.457, 1e-2)
+        assert system_level_metrics['E']['rouge-2_jk']['recall'] == pytest.approx(13.341, 1e-2)
+        assert system_level_metrics['2']['rouge-2_jk']['recall'] == pytest.approx(33.133, 1e-2)
+        assert system_level_metrics['34']['rouge-2_jk']['recall'] == pytest.approx(12.184, 1e-2)
+
+        # ROUGE/rougeSU4_A.m.avg
+        assert system_level_metrics['2']['rouge-su4']['recall'] == pytest.approx(34.421, 1e-2)
+        assert system_level_metrics['40']['rouge-su4']['recall'] == pytest.approx(15.101, 1e-2)
+        assert system_level_metrics['34']['rouge-su4']['recall'] == pytest.approx(15.030, 1e-2)
+        assert system_level_metrics['35']['rouge-su4']['recall'] == pytest.approx(14.487, 1e-2)
+        assert system_level_metrics['51']['rouge-su4']['recall'] == pytest.approx(14.165, 1e-2)
+
+        # ROUGE/rougeSU4_A.jk.m.avg
+        assert system_level_metrics['C']['rouge-su4_jk']['recall'] == pytest.approx(18.355, 1e-2)
+        assert system_level_metrics['H']['rouge-su4_jk']['recall'] == pytest.approx(17.199, 1e-2)
+        assert system_level_metrics['E']['rouge-su4_jk']['recall'] == pytest.approx(16.917, 1e-2)
+        assert system_level_metrics['2']['rouge-su4_jk']['recall'] == pytest.approx(34.399, 1e-2)
+        assert system_level_metrics['40']['rouge-su4_jk']['recall'] == pytest.approx(15.131, 1e-2)
+
+        # manual/manual.model.A.avg
+        assert system_level_metrics['A']['num_scus_jk'] == pytest.approx(10.364, 1e-2)
+        assert system_level_metrics['B']['num_scus_jk'] == pytest.approx(9.5, 1e-2)
+        assert system_level_metrics['C']['num_scus_jk'] == pytest.approx(12.364, 1e-2)
+
+        assert system_level_metrics['A']['modified_pyramid_score_jk'] == pytest.approx(0.685, 1e-2)
+        assert system_level_metrics['B']['modified_pyramid_score_jk'] == pytest.approx(0.616, 1e-2)
+        assert system_level_metrics['C']['modified_pyramid_score_jk'] == pytest.approx(0.720, 1e-2)
+
+        assert system_level_metrics['A']['linguistic_quality'] == pytest.approx(8.636, 1e-2)
+        assert system_level_metrics['B']['linguistic_quality'] == pytest.approx(9.136, 1e-2)
+        assert system_level_metrics['C']['linguistic_quality'] == pytest.approx(9.136, 1e-2)
+
+        assert system_level_metrics['A']['overall_responsiveness'] == pytest.approx(8.455, 1e-2)
+        assert system_level_metrics['B']['overall_responsiveness'] == pytest.approx(8.727, 1e-2)
+        assert system_level_metrics['C']['overall_responsiveness'] == pytest.approx(9.318, 1e-2)
+
+        # manual/manual.peer.A.avg
+        assert system_level_metrics['1']['modified_pyramid_score'] == pytest.approx(0.175, 1e-2)
+        assert system_level_metrics['2']['modified_pyramid_score'] == pytest.approx(0.646, 1e-2)
+        assert system_level_metrics['3']['modified_pyramid_score'] == pytest.approx(0.358, 1e-2)
+
+        assert system_level_metrics['1']['num_scus'] == pytest.approx(3.182, 1e-2)
+        assert system_level_metrics['2']['num_scus'] == pytest.approx(11.977, 1e-2)
+        assert system_level_metrics['3']['num_scus'] == pytest.approx(6.00, 1e-2)
+
+        assert system_level_metrics['1']['num_repetitions'] == pytest.approx(1.318, 1e-2)
+        assert system_level_metrics['2']['num_repetitions'] == pytest.approx(2.455, 1e-2)
+        assert system_level_metrics['3']['num_repetitions'] == pytest.approx(1.568, 1e-2)
+
+        assert system_level_metrics['1']['modified_pyramid_score_jk'] == pytest.approx(0.172, 1e-2)
+        assert system_level_metrics['2']['modified_pyramid_score_jk'] == pytest.approx(0.635, 1e-2)
+        assert system_level_metrics['3']['modified_pyramid_score_jk'] == pytest.approx(0.352, 1e-2)
+
+        assert system_level_metrics['1']['linguistic_quality'] == pytest.approx(6.705, 1e-2)
+        assert system_level_metrics['2']['linguistic_quality'] == pytest.approx(5.477, 1e-2)
+        assert system_level_metrics['3']['linguistic_quality'] == pytest.approx(7.477, 1e-2)
+
+        assert system_level_metrics['1']['overall_responsiveness'] == pytest.approx(3.636, 1e-2)
+        assert system_level_metrics['2']['overall_responsiveness'] == pytest.approx(6.364, 1e-2)
+        assert system_level_metrics['3']['overall_responsiveness'] == pytest.approx(6.341, 1e-2)
+
+        # BE/simple_A.m.hm.avg
+        assert system_level_metrics['2']['rouge-be-hm']['recall'] == pytest.approx(24.820, 1e-2)
+        assert system_level_metrics['34']['rouge-be-hm']['recall'] == pytest.approx(6.356, 1e-2)
+        assert system_level_metrics['40']['rouge-be-hm']['recall'] == pytest.approx(6.321, 1e-2)
+        assert system_level_metrics['45']['rouge-be-hm']['recall'] == pytest.approx(5.899, 1e-2)
+        assert system_level_metrics['4']['rouge-be-hm']['recall'] == pytest.approx(5.843, 1e-2)
+
+        # BE/simplejk_A.m.hm.avg
+        assert system_level_metrics['C']['rouge-be-hm_jk']['recall'] == pytest.approx(7.876, 1e-2)
+        assert system_level_metrics['E']['rouge-be-hm_jk']['recall'] == pytest.approx(6.909, 1e-2)
+        assert system_level_metrics['F']['rouge-be-hm_jk']['recall'] == pytest.approx(6.840, 1e-2)
+        assert system_level_metrics['2']['rouge-be-hm_jk']['recall'] == pytest.approx(24.830, 1e-2)
+        assert system_level_metrics['34']['rouge-be-hm_jk']['recall'] == pytest.approx(6.379, 1e-2)
+
+    @pytest.mark.skipif(not os.path.exists(_metrics_B_file_path), reason='TAC 2009-B metrics file does not exist')
+    def test_system_level_B(self):
+        summary_level_metrics = JsonlReader(_metrics_B_file_path).read()
+        system_level_metrics = aggregate_metrics(summary_level_metrics, 'summarizer_id')
+
+        # Check a few metrics to make sure they are equal to what's in the NIST files
+        # ROUGE/rouge2_B.m.avg
+        assert system_level_metrics['2']['rouge-2']['recall'] == pytest.approx(31.956, 1e-2)
+        assert system_level_metrics['34']['rouge-2']['recall'] == pytest.approx(10.386, 1e-2)
+        assert system_level_metrics['40']['rouge-2']['recall'] == pytest.approx(10.373, 1e-2)
+        assert system_level_metrics['35']['rouge-2']['recall'] == pytest.approx(10.104, 1e-2)
+        assert system_level_metrics['3']['rouge-2']['recall'] == pytest.approx(9.820, 1e-2)
+
+        # ROUGE/rouge2_B.jk.m.avg
+        # C is off by a bit?
+        # assert system_level_metrics['C']['rouge-2_jk']['recall'] == pytest.approx(12.550, 1e-2)
+        assert system_level_metrics['H']['rouge-2_jk']['recall'] == pytest.approx(12.436, 1e-2)
+        assert system_level_metrics['E']['rouge-2_jk']['recall'] == pytest.approx(11.001, 1e-2)
+        assert system_level_metrics['2']['rouge-2_jk']['recall'] == pytest.approx(31.932, 1e-2)
+        assert system_level_metrics['34']['rouge-2_jk']['recall'] == pytest.approx(10.417, 1e-2)
+
+        # ROUGE/rougeSU4_B.m.avg
+        assert system_level_metrics['2']['rouge-su4']['recall'] == pytest.approx(33.688, 1e-2)
+        assert system_level_metrics['40']['rouge-su4']['recall'] == pytest.approx(13.948, 1e-2)
+        assert system_level_metrics['34']['rouge-su4']['recall'] == pytest.approx(13.851, 1e-2)
+        assert system_level_metrics['35']['rouge-su4']['recall'] == pytest.approx(13.839, 1e-2)
+        assert system_level_metrics['51']['rouge-su4']['recall'] == pytest.approx(13.650, 1e-2)
+
+        # ROUGE/rougeSU4_B.jk.m.avg
+        assert system_level_metrics['C']['rouge-su4_jk']['recall'] == pytest.approx(16.386, 1e-2)
+        assert system_level_metrics['H']['rouge-su4_jk']['recall'] == pytest.approx(16.602, 1e-2)
+        assert system_level_metrics['E']['rouge-su4_jk']['recall'] == pytest.approx(15.152, 1e-2)
+        assert system_level_metrics['2']['rouge-su4_jk']['recall'] == pytest.approx(33.668, 1e-2)
+        assert system_level_metrics['40']['rouge-su4_jk']['recall'] == pytest.approx(13.959, 1e-2)
+
+        # manual/manual.model.B.avg
+        assert system_level_metrics['A']['num_scus_jk'] == pytest.approx(6.455, 1e-2)
+        assert system_level_metrics['B']['num_scus_jk'] == pytest.approx(8.591, 1e-2)
+        assert system_level_metrics['C']['num_scus_jk'] == pytest.approx(8.545, 1e-2)
+
+        assert system_level_metrics['A']['modified_pyramid_score_jk'] == pytest.approx(0.481, 1e-2)
+        assert system_level_metrics['B']['modified_pyramid_score_jk'] == pytest.approx(0.663, 1e-2)
+        assert system_level_metrics['C']['modified_pyramid_score_jk'] == pytest.approx(0.640, 1e-2)
+
+        assert system_level_metrics['A']['linguistic_quality'] == pytest.approx(8.727, 1e-2)
+        assert system_level_metrics['B']['linguistic_quality'] == pytest.approx(8.545, 1e-2)
+        assert system_level_metrics['C']['linguistic_quality'] == pytest.approx(9.364, 1e-2)
+
+        assert system_level_metrics['A']['overall_responsiveness'] == pytest.approx(8.364, 1e-2)
+        assert system_level_metrics['B']['overall_responsiveness'] == pytest.approx(8.318, 1e-2)
+        assert system_level_metrics['C']['overall_responsiveness'] == pytest.approx(9.136, 1e-2)
+
+        # manual/manual.peer.B.avg
+        assert system_level_metrics['1']['modified_pyramid_score'] == pytest.approx(0.160, 1e-2)
+        assert system_level_metrics['2']['modified_pyramid_score'] == pytest.approx(0.690, 1e-2)
+        assert system_level_metrics['3']['modified_pyramid_score'] == pytest.approx(0.329, 1e-2)
+
+        assert system_level_metrics['1']['num_scus'] == pytest.approx(2.386, 1e-2)
+        assert system_level_metrics['2']['num_scus'] == pytest.approx(9.886, 1e-2)
+        assert system_level_metrics['3']['num_scus'] == pytest.approx(4.545, 1e-2)
+
+        assert system_level_metrics['1']['num_repetitions'] == pytest.approx(0.841, 1e-2)
+        assert system_level_metrics['2']['num_repetitions'] == pytest.approx(1.955, 1e-2)
+        assert system_level_metrics['3']['num_repetitions'] == pytest.approx(0.955, 1e-2)
+
+        assert system_level_metrics['1']['modified_pyramid_score_jk'] == pytest.approx(0.158, 1e-2)
+        assert system_level_metrics['2']['modified_pyramid_score_jk'] == pytest.approx(0.677, 1e-2)
+        assert system_level_metrics['3']['modified_pyramid_score_jk'] == pytest.approx(0.324, 1e-2)
+
+        assert system_level_metrics['1']['linguistic_quality'] == pytest.approx(6.455, 1e-2)
+        assert system_level_metrics['2']['linguistic_quality'] == pytest.approx(5.886, 1e-2)
+        assert system_level_metrics['3']['linguistic_quality'] == pytest.approx(7.250, 1e-2)
+
+        assert system_level_metrics['1']['overall_responsiveness'] == pytest.approx(4.318, 1e-2)
+        assert system_level_metrics['2']['overall_responsiveness'] == pytest.approx(6.182, 1e-2)
+        assert system_level_metrics['3']['overall_responsiveness'] == pytest.approx(6.114, 1e-2)
+
+        # BE/simple_B.m.hm.avg
+        assert system_level_metrics['2']['rouge-be-hm']['recall'] == pytest.approx(25.041, 1e-2)
+        assert system_level_metrics['24']['rouge-be-hm']['recall'] == pytest.approx(6.389, 1e-2)
+        assert system_level_metrics['40']['rouge-be-hm']['recall'] == pytest.approx(6.162, 1e-2)
+        assert system_level_metrics['34']['rouge-be-hm']['recall'] == pytest.approx(6.118, 1e-2)
+        assert system_level_metrics['35']['rouge-be-hm']['recall'] == pytest.approx(5.813, 1e-2)
+
+        # BE/simplejk_B.m.hm.avg
+        # F is off by a little
+        assert system_level_metrics['C']['rouge-be-hm_jk']['recall'] == pytest.approx(6.795, 1e-2)
+        assert system_level_metrics['H']['rouge-be-hm_jk']['recall'] == pytest.approx(7.040, 1e-2)
+        assert system_level_metrics['F']['rouge-be-hm_jk']['recall'] == pytest.approx(6.094, 1e-1)
+        assert system_level_metrics['2']['rouge-be-hm_jk']['recall'] == pytest.approx(25.042, 1e-2)
+        assert system_level_metrics['34']['rouge-be-hm_jk']['recall'] == pytest.approx(6.134, 1e-2)
