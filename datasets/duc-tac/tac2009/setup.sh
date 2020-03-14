66c90bf2087820e206762043f08f07ea3ca1a153
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-13 22:52:34 -0400

    Uncommenting lines

--- datasets/duc-tac/tac2009/setup.sh
+++ datasets/duc-tac/tac2009/setup.sh
@@ -7,11 +7,11 @@ root=$1
 
 version="v1.0"
 
-# python -m sacrerouge.datasets.duc_tac.tac2009.task1 \
-#   ${root}/from-nist/TAC2009_Summarization_Documents.tgz \
-#   ${root}/scrapes/tac.nist.gov/protected/past-aquaint2/2009/UpdateSumm09_eval.tar.gz \
-#   ${root}/scrapes/tac.nist.gov/protected/past-aquaint2/2009/UpdateSumm09_test_topics.xml.txt \
-#   datasets/duc-tac/tac2009/${version}
+python -m sacrerouge.datasets.duc_tac.tac2009.task1 \
+  ${root}/from-nist/TAC2009_Summarization_Documents.tgz \
+  ${root}/scrapes/tac.nist.gov/protected/past-aquaint2/2009/UpdateSumm09_eval.tar.gz \
+  ${root}/scrapes/tac.nist.gov/protected/past-aquaint2/2009/UpdateSumm09_test_topics.xml.txt \
+  datasets/duc-tac/tac2009/${version}
 
 python -m sacrerouge.datasets.duc_tac.tac2009.metrics \
   ${root}/scrapes/tac.nist.gov/protected/past-aquaint2/2009/UpdateSumm09_eval.tar.gz \
