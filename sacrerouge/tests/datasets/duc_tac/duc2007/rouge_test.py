229ba01e349c326c03a97c6096b7340d3c8312b5
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-18 17:28:13 -0400

    Adding DUC 2007 tests

---
+++ sacrerouge/tests/datasets/duc_tac/duc2007/rouge_test.py
@@ -0,0 +1,110 @@
+import os
+import pytest
+import unittest
+from typing import Any, Dict, List
+
+from sacrerouge.io import JsonlReader
+from sacrerouge.metrics import Rouge
+from sacrerouge.score import run_jackknifing
+
+_task1_summaries_file_path = 'datasets/duc-tac/duc2007/v1.0/task1.summaries.jsonl'
+_task1_metrics_file_path = 'datasets/duc-tac/duc2007/v1.0/task1.metrics.jsonl'
+
+_task2_summaries_file_path = 'datasets/duc-tac/duc2007/v1.0/task2.A-B-C.summaries.jsonl'
+_task2_metrics_file_path = 'datasets/duc-tac/duc2007/v1.0/task2.A-B-C.metrics.jsonl'
+
+
+class TestDUC2007Rouge(unittest.TestCase):
+    def _filter_by_type(self,
+                        instances: List[Dict[str, Any]],
+                        metrics_list: List[Dict[str, Any]],
+                        summarizer_type: str):
+        filtered_instances = []
+        filtered_metrics = []
+        for instance, metrics in zip(instances, metrics_list):
+            if instance['summarizer_type'] == summarizer_type:
+                filtered_instances.append(instance)
+                filtered_metrics.append(metrics)
+        return filtered_instances, filtered_metrics
+
+    @pytest.mark.skipif(not os.path.exists(_task1_summaries_file_path), reason='DUC 2007 task 1 summaries file does not exist')
+    @pytest.mark.skipif(not os.path.exists(_task1_metrics_file_path), reason='DUC 2007 task 1 metrics file does not exist')
+    def test_task1_rouge(self):
+        # Test the first several instances in the DUC 2007 data to ensure that
+        # our computation of ROUGE matches the values released by NIST. In this year,
+        # they did not release the raw output for the non-jackknifing runs, so
+        # we cannot compare those scores
+        instances = JsonlReader(_task1_summaries_file_path).read()
+        metrics_list = JsonlReader(_task1_metrics_file_path).read()
+        metric_names = ['rouge-1', 'rouge-2', 'rouge-3', 'rouge-4', 'rouge-l', 'rouge-su4', 'rouge-w-1.2']
+        submetrics = ['precision', 'recall', 'f1']
+        rouge = Rouge(max_ngram=4,
+                      use_porter_stemmer=True,
+                      remove_stopwords=False,
+                      compute_rouge_l=True,
+                      skip_bigram_gap_length=4,
+                      wlcs_weight=1.2)
+
+        peer_instances, peer_metrics = self._filter_by_type(instances, metrics_list, 'peer')
+        reference_instances, reference_metrics = self._filter_by_type(instances, metrics_list, 'reference')
+
+        num_to_check = 25
+        for instance, metrics in zip(peer_instances[:num_to_check], peer_metrics[:num_to_check]):
+            summary = instance['summary']['text']
+            references = [reference['text'] for reference in instance['references']]
+            actual_metrics = rouge.score(summary, references)
+
+            actual_metrics = run_jackknifing([rouge], summary, references)
+            for metric in metric_names:
+                for submetric in submetrics:
+                    assert actual_metrics[metric + '_jk'][submetric] == metrics['metrics'][metric + '_jk'][submetric]
+
+        for instance, metrics in zip(reference_instances[:num_to_check], reference_metrics[:num_to_check]):
+            summary = instance['summary']['text']
+            references = [reference['text'] for reference in instance['references']]
+            actual_metrics = rouge.score(summary, references)
+
+            for metric in metric_names:
+                for submetric in submetrics:
+                    assert actual_metrics[metric][submetric] == metrics['metrics'][metric + '_jk'][submetric]
+
+    @pytest.mark.skipif(not os.path.exists(_task2_summaries_file_path), reason='DUC 2007 task 2 summaries file does not exist')
+    @pytest.mark.skipif(not os.path.exists(_task2_metrics_file_path), reason='DUC 2007 task 2 metrics file does not exist')
+    def test_task2_rouge(self):
+        # Test the first several instances in the DUC 2007 data to ensure that
+        # our computation of ROUGE matches the values released by NIST. In this year,
+        # they did not release the raw output for the non-jackknifing runs, so
+        # we cannot compare those scores
+        instances = JsonlReader(_task2_summaries_file_path).read()
+        metrics_list = JsonlReader(_task2_metrics_file_path).read()
+        metric_names = ['rouge-1', 'rouge-2', 'rouge-3', 'rouge-4', 'rouge-l', 'rouge-su4', 'rouge-w-1.2']
+        submetrics = ['precision', 'recall', 'f1']
+        rouge = Rouge(max_ngram=4,
+                      use_porter_stemmer=True,
+                      remove_stopwords=False,
+                      compute_rouge_l=True,
+                      skip_bigram_gap_length=4,
+                      wlcs_weight=1.2)
+
+        peer_instances, peer_metrics = self._filter_by_type(instances, metrics_list, 'peer')
+        reference_instances, reference_metrics = self._filter_by_type(instances, metrics_list, 'reference')
+
+        num_to_check = 25
+        for instance, metrics in zip(peer_instances[:num_to_check], peer_metrics[:num_to_check]):
+            summary = instance['summary']['text']
+            references = [reference['text'] for reference in instance['references']]
+            actual_metrics = rouge.score(summary, references)
+
+            actual_metrics = run_jackknifing([rouge], summary, references)
+            for metric in metric_names:
+                for submetric in submetrics:
+                    assert actual_metrics[metric + '_jk'][submetric] == pytest.approx(metrics['metrics'][metric + '_jk'][submetric], abs=1e-2)
+
+        for instance, metrics in zip(reference_instances[:num_to_check], reference_metrics[:num_to_check]):
+            summary = instance['summary']['text']
+            references = [reference['text'] for reference in instance['references']]
+            actual_metrics = rouge.score(summary, references)
+
+            for metric in metric_names:
+                for submetric in submetrics:
+                    assert actual_metrics[metric][submetric] == pytest.approx(metrics['metrics'][metric + '_jk'][submetric], abs=1e-2)
