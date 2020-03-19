2bb43a5182951c461c62e2643bd97d2f9fbe5cfe
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-18 21:52:10 -0400

    Adding DUC 2005 metrics and tests

--- datasets/duc-tac/duc2005/setup.sh
+++ datasets/duc-tac/duc2005/setup.sh
@@ -12,3 +12,7 @@ python -m sacrerouge.datasets.duc_tac.duc2005.task1 \
   ${root}/scrapes/duc.nist.gov/past_duc/duc2005/results/NIST/results.tar \
   ${root}/scrapes/duc.nist.gov/past_duc/duc2005/testdata/duc2005_topics.sgml \
   datasets/duc-tac/duc2005/${version}
+
+python -m sacrerouge.datasets.duc_tac.duc2005.metrics \
+  ${root}/scrapes/duc.nist.gov/past_duc/duc2005/results/NIST/results.tar \
+  datasets/duc-tac/duc2005/${version}
---
+++ sacrerouge/datasets/duc_tac/duc2005/metrics.py
@@ -0,0 +1,152 @@
+import argparse
+import tarfile
+from collections import defaultdict
+from typing import Dict, List
+
+from sacrerouge.common.util import merge_dict
+from sacrerouge.io import JsonlWriter
+
+
+def load_summaries(results_tar: str):
+    summaries = defaultdict(dict)
+    with tarfile.open(results_tar, 'r') as tar:
+        for member in tar.getmembers():
+            if member.name.startswith('results/ROUGE/models/') or member.name.startswith('results/ROUGE/peers/'):
+                parts = member.name.split('/')[-1].split('.')
+                instance_id = parts[0].lower()
+                summarizer_id = parts[-1]
+                if summarizer_id.isalpha():
+                    summarizer_type = 'reference'
+                else:
+                    summarizer_type = 'peer'
+                sentences = tar.extractfile(member).read().decode(errors='replace').splitlines()
+                summary = {
+                    'summarizer_id': summarizer_id,
+                    'summarizer_type': summarizer_type,
+                    'text': sentences
+                }
+                if summarizer_id in summaries[instance_id]:
+                    assert summaries[instance_id][summarizer_id] == summary
+                summaries[instance_id][summarizer_id] = summary
+    return summaries
+
+
+def load_rouge_jk_output(eval_tar: str, file_path: str):
+    jk_metrics = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+    with tarfile.open(eval_tar, 'r') as tar:
+        lines = tar.extractfile(file_path).read().decode().splitlines()
+        for line in lines:
+            columns = line.split()
+            if len(columns) == 7 and columns[2] == 'Eval':
+                summarizer_id = columns[0]
+                rouge_metric = columns[1].lower() + '_jk'
+                filename = columns[3].split('.')
+                instance_id = filename[0].lower()
+                summarizer_id = filename[-1]
+
+                recall = float(columns[4][2:]) * 100
+                precision = float(columns[5][2:]) * 100
+                f1 = float(columns[6][2:]) * 100
+                jk_metrics[instance_id][summarizer_id][rouge_metric]['recall'].append(recall)
+                jk_metrics[instance_id][summarizer_id][rouge_metric]['precision'].append(precision)
+                jk_metrics[instance_id][summarizer_id][rouge_metric]['f1'].append(f1)
+
+        metrics = defaultdict(lambda: defaultdict(dict))
+        for instance_id in jk_metrics.keys():
+            for summarizer_id in jk_metrics[instance_id].keys():
+                for rouge_metric in jk_metrics[instance_id][summarizer_id].keys():
+                    recalls = jk_metrics[instance_id][summarizer_id][rouge_metric]['recall']
+                    precisions = jk_metrics[instance_id][summarizer_id][rouge_metric]['precision']
+                    f1s = jk_metrics[instance_id][summarizer_id][rouge_metric]['f1']
+                    metrics[instance_id][summarizer_id][rouge_metric] = {
+                        'recall': sum(recalls) / len(recalls),
+                        'precision': sum(precisions) / len(precisions),
+                        'f1': sum(f1s) / len(f1s)
+                    }
+    return metrics
+
+
+def load_responsiveness_table(eval_tar: str):
+    judgments = defaultdict(lambda: defaultdict(dict))
+    with tarfile.open(eval_tar, 'r') as tar:
+        lines = tar.extractfile('results/responsiveness/responsiveness.table').read().decode().splitlines()
+        for line in lines[6:]:
+            columns = line.split()
+            instance_id = columns[0].lower()
+            summarizer_id = columns[3]
+            score = int(columns[4])
+            judgments[instance_id][summarizer_id]['responsiveness'] = score
+    return judgments
+
+
+def load_linguistic_quality_table(eval_tar: str):
+    judgments = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
+    with tarfile.open(eval_tar, 'r') as tar:
+        lines = tar.extractfile('results/linguistic_quality/linguistic_quality.table').read().decode().splitlines()
+        for line in lines[7:]:
+            columns = line.split()
+            instance_id = columns[0].lower()
+            summarizer_id = columns[3]
+            question = columns[4]
+            score = int(columns[5])
+            judgments[instance_id][summarizer_id]['linguistic_quality'][f'Q{question}'] = score
+    return judgments
+
+
+def get_references(summaries, instance_id, summarizer_id):
+    summarizer_ids = list(summaries[instance_id].keys())
+    reference_ids = list(filter(lambda sid: sid.isalpha(), summarizer_ids))
+
+    references = []
+    for reference_id in reference_ids:
+        if summarizer_id == reference_id:
+            continue
+        references.append(summaries[instance_id][reference_id])
+    return references
+
+
+def save_metrics(summaries: Dict[str, Dict[str, List[str]]],
+                 metrics: Dict[str, Dict[str, List[int]]],
+                 output_dir: str):
+    with JsonlWriter(f'{output_dir}/task1.summaries.jsonl') as out_summaries:
+        with JsonlWriter(f'{output_dir}/task1.metrics.jsonl') as out_metrics:
+            for instance_id in sorted(summaries.keys()):
+                for summarizer_id in summaries[instance_id].keys():
+                    summary = summaries[instance_id][summarizer_id]
+                    instance_metrics = metrics[instance_id][summarizer_id]
+                    references = get_references(summaries, instance_id, summarizer_id)
+
+                    out_summaries.write({
+                        'instance_id': instance_id,
+                        'summarizer_id': summarizer_id,
+                        'summarizer_type': summary['summarizer_type'],
+                        'summary': summary,
+                        'references': references
+                    })
+                    out_metrics.write({
+                        'instance_id': instance_id,
+                        'summarizer_id': summarizer_id,
+                        'summarizer_type': summary['summarizer_type'],
+                        'metrics': instance_metrics
+                    })
+
+
+def main(args):
+    summaries = load_summaries(args.eval_tar)
+    rouge = load_rouge_jk_output(args.eval_tar, 'results/ROUGE/rougejk.m.out')
+    responsiveness = load_responsiveness_table(args.eval_tar)
+    linguistic_quality = load_linguistic_quality_table(args.eval_tar)
+
+    metrics = rouge
+    merge_dict(metrics, responsiveness)
+    merge_dict(metrics, linguistic_quality)
+
+    save_metrics(summaries, metrics, args.output_dir)
+
+
+if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument('eval_tar')
+    argp.add_argument('output_dir')
+    args = argp.parse_args()
+    main(args)
