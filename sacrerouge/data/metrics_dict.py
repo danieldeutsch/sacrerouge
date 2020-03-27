a0c0d4f9c435a8f3bd12e6d27b54fcd9fd0d42e2
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-26 22:00:48 -0400

    Adding the MetricsDict type

---
+++ sacrerouge/data/metrics_dict.py
@@ -0,0 +1,101 @@
+import copy
+from typing import Dict, List, Optional, Union
+
+ValueType = Union['MetricsDict', float, List[float]]
+
+
+class MetricsDict(dict):
+    def __init__(self, initial_dict: Optional[Union['MetricsDict', Dict]] = None) -> None:
+        super().__init__()
+        if initial_dict:
+            for key, value in initial_dict.items():
+                self[key] = value
+
+    def __setitem__(self, key: str, value: Union[ValueType, Dict]) -> None:
+        # If it's a dictionary, we need to convert it to a MetricsDict
+        if isinstance(value, dict):
+            value_dict = MetricsDict()
+            for k, v in value.items():
+                value_dict[k] = v
+            value = value_dict
+
+        return dict.__setitem__(self, key, value)
+
+    def __getitem__(self, key: str) -> ValueType:
+        if not dict.__contains__(self, key):
+            dict.__setitem__(self, key, MetricsDict())
+        return dict.__getitem__(self, key)
+
+    def flatten_keys(self) -> 'MetricsDict':
+        def _recursive_iterate(metrics_dict: MetricsDict, prefix: List[str]):
+            for key, value in metrics_dict.items():
+                if isinstance(value, MetricsDict):
+                    yield from _recursive_iterate(value, prefix + [key])
+                else:
+                    yield (prefix + [key], value)
+
+        result = MetricsDict()
+        for path, value in _recursive_iterate(self, []):
+            name = '_'.join(path)
+            result[name] = value
+        return result
+
+    def average_values(self) -> 'MetricsDict':
+        result = MetricsDict()
+        for key, value in self.items():
+            if isinstance(value, MetricsDict):
+                result[key] = value.average_values()
+            elif isinstance(value, list):
+                result[key] = sum(value) / len(value)
+            else:
+                result[key] = value
+        return result
+
+    def _add_to(self, target: 'MetricsDict') -> 'MetricsDict':
+        # We created this method so that both __add__ and __radd__ can create
+        # an intermediate cumulative MetricsDict and use this method to add to it.
+        # Otherwise, it's impossible for __add__ to know if it's adding to the
+        # cumulative MetricsDict or the other actual value.
+        assert self.keys() == target.keys()
+        for key, value in self.items():
+            target[key] += value
+        return target
+
+    def __add__(self, other: 'MetricsDict') -> 'MetricsDict':
+        assert self.keys() == other.keys()
+        total = copy.deepcopy(other)
+        return self._add_to(total)
+
+    def __radd__(self, other: Union[int, 'MetricsDict']) -> 'MetricsDict':
+        if other == 0:
+            return copy.deepcopy(self)
+        else:
+            return self._add_to(other)
+
+    def __truediv__(self, denominator: float) -> 'MetricsDict':
+        result = MetricsDict()
+        for key, value in self.items():
+            result[key] = value / denominator
+        return result
+
+
+def main():
+    import jsons
+
+    d = MetricsDict()
+    d['a']['b']['c'] = 5
+
+    e = MetricsDict()
+    e['a']['b']['c'] = 2
+
+    f = MetricsDict()
+    f['a']['b']['c'] = 1
+
+    f = MetricsDict()
+    f['a']['b']['c'] = [1, 5]
+
+    g = jsons.loads(jsons.dumps(f), MetricsDict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ sacrerouge/tests/data/metrics_dict_test.py
@@ -0,0 +1,49 @@
+import unittest
+
+from sacrerouge.data import MetricsDict
+
+
+class TestMetricsDict(unittest.TestCase):
+    def test_get_set_item(self):
+        metrics = MetricsDict()
+
+        metrics['a'] = 4
+        assert metrics['a'] == 4
+
+        metrics['b']['c'] = [1, 2]
+        assert metrics['b']['c'] == [1, 2]
+        assert isinstance(metrics['b'], MetricsDict)
+
+        metrics['d'] = {'e': 4, 'f': {'g': 4}}
+        assert metrics['d'] == {'e': 4, 'f': {'g': 4}}
+        assert isinstance(metrics['d'], MetricsDict)
+        assert isinstance(metrics['d']['f'], MetricsDict)
+
+    def test_add(self):
+        a = MetricsDict({'k1': 1, 'k2': {'k3': 4}})
+        b = MetricsDict({'k1': 2, 'k2': {'k3': 5}})
+        c = MetricsDict({'k1': 3, 'k2': {'k3': 6}})
+
+        assert a + b == {'k1': 3, 'k2': {'k3': 9}}
+        assert a == {'k1': 1, 'k2': {'k3': 4}}
+        assert b == {'k1': 2, 'k2': {'k3': 5}}
+
+        assert sum([a, b, c]) == {'k1': 6, 'k2': {'k3': 15}}
+        assert a == {'k1': 1, 'k2': {'k3': 4}}
+        assert b == {'k1': 2, 'k2': {'k3': 5}}
+        assert c == {'k1': 3, 'k2': {'k3': 6}}
+
+    def test_divide(self):
+        metrics = MetricsDict({'k1': 1, 'k2': {'k3': 4}})
+        assert metrics / 2 == {'k1': 0.5, 'k2': {'k3': 2.0}}
+        assert metrics == {'k1': 1, 'k2': {'k3': 4}}
+
+    def test_average_values(self):
+        metrics = MetricsDict({'k1': 1, 'k2': {'k3': [1, 2, 3]}})
+        assert metrics.average_values() == {'k1': 1, 'k2': {'k3': 2.0}}
+        assert metrics == {'k1': 1, 'k2': {'k3': [1, 2, 3]}}
+
+    def test_flatten_keys(self):
+        metrics = MetricsDict({'k1': 1, 'k2': {'k3': 4}})
+        assert metrics.flatten_keys() == {'k1': 1, 'k2_k3': 4}
+        assert metrics == {'k1': 1, 'k2': {'k3': 4}}
