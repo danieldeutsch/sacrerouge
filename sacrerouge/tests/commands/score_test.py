ebd27cde47b6341eccd822542fa270ea31898c3b
Daniel Deutsch <danfdeutsch@gmail.com>
2020-03-27 15:23:37 -0400

    Updating

--- sacrerouge/tests/commands/score_test.py
+++ sacrerouge/tests/commands/score_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sacrerouge.commands.score import average_jackknifing_metrics, get_jackknifing_references_list
+from sacrerouge.commands.score import get_jackknifing_references_list
 
 
 class TestScore(unittest.TestCase):
@@ -13,29 +13,3 @@ class TestScore(unittest.TestCase):
         assert ['B', 'C', 'D'] in jk_references_list
         assert ['A', 'C', 'D'] in jk_references_list
         assert ['A', 'B', 'D'] in jk_references_list
-
-    def test_average_jackknifing_metrics(self):
-        metrics_list = [
-            {
-                'a': 2,
-                'b': {
-                    'c': 4,
-                    'd': 6
-                }
-            },
-            {
-                'a': 4,
-                'b': {
-                    'c': 10,
-                    'd': 12
-                }
-            }
-        ]
-        average = average_jackknifing_metrics(metrics_list)
-        assert average == {
-            'a': 3,
-            'b': {
-                'c': 7,
-                'd': 9
-            }
-        }
