06b03ce81f5ec9dfaf0b99b86ae3871d1036c9e4
Daniel Deutsch <danfdeutsch@gmail.com>
2020-04-09 17:31:06 -0400

    Adding flag to check if MoverScore exists

--- sacrerouge/tests/metrics/moverscore_test.py
+++ sacrerouge/tests/metrics/moverscore_test.py
@@ -4,14 +4,16 @@ import unittest
 
 from sacrerouge.common.testing import FIXTURES_ROOT, load_references, load_summaries
 from sacrerouge.metrics import MoverScore
+from sacrerouge.metrics.moverscore import MOVERSCORE_EXISTS
 
 _duc2004_file_path = 'datasets/duc-tac/duc2004/task2.jsonl'
 _centroid_file_path = f'{FIXTURES_ROOT}/data/hong2014/centroid.jsonl'
 
 
+@pytest.mark.skipif(not MOVERSCORE_EXISTS, reason='MoverScore not setup')
+@pytest.mark.skipif(not os.path.exists(_duc2004_file_path), reason='DUC 2004 data does not exist')
+@pytest.mark.skipif(not os.path.exists(_centroid_file_path), reason='Hong 2014 data does not exist')
 class TestMoverScore(unittest.TestCase):
-    @pytest.mark.skipif(not os.path.exists(_duc2004_file_path), reason='DUC 2004 data does not exist')
-    @pytest.mark.skipif(not os.path.exists(_centroid_file_path), reason='Hong 2014 data does not exist')
     def test_mover_score_runs(self):
         # These numbers were not checked to be correct, but will detect if anything
         # changes in the code
