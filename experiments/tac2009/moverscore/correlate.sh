5a6aa9be629e3d0de07bcdd01dad7ba9c2caeca4
Daniel Deutsch <danfdeutsch@gmail.com>
2020-04-09 21:27:48 -0400

    Adding pyramid

--- experiments/tac2009/moverscore/correlate.sh
+++ experiments/tac2009/moverscore/correlate.sh
@@ -23,4 +23,25 @@ for split in 'A' 'B' 'A-B'; do
     --summarizer-type all \
     --output-file ${DIR}/output/${split}/all/responsiveness_moverscore.json \
     --silent
+
+  python -m sacrerouge correlate \
+    --metrics-jsonl-files datasets/duc-tac/tac2008/v1.0/task1.${split}.metrics.jsonl ${DIR}/output/scores.${split}.jsonl \
+    --metrics modified_pyramid_score MoverScore \
+    --summarizer-type peer \
+    --output-file ${DIR}/output/${split}/peers/pyramid_moverscore.json \
+    --silent
+
+  python -m sacrerouge correlate \
+    --metrics-jsonl-files datasets/duc-tac/tac2008/v1.0/task1.${split}.metrics.jsonl ${DIR}/output/scores.${split}.jsonl \
+    --metrics modified_pyramid_score_jk MoverScore_jk \
+    --summarizer-type reference \
+    --output-file ${DIR}/output/${split}/references/pyramid_moverscore.json \
+    --silent
+
+  python -m sacrerouge correlate \
+    --metrics-jsonl-files datasets/duc-tac/tac2008/v1.0/task1.${split}.metrics.jsonl ${DIR}/output/scores.${split}.jsonl \
+    --metrics modified_pyramid_score_jk MoverScore_jk \
+    --summarizer-type all \
+    --output-file ${DIR}/output/${split}/all/pyramid_moverscore.json \
+    --silent
 done
