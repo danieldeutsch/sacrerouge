f30aad1e58f58ae3b250296c8b507fad413598f5
Daniel Deutsch <danfdeutsch@gmail.com>
2020-04-12 13:45:16 -0400

    Typo

--- experiments/duc2005/sum-qe/correlate.sh
+++ experiments/duc2005/sum-qe/correlate.sh
@@ -5,7 +5,7 @@ mkdir -p ${DIR}/output
 for q in 'Q1' 'Q2' 'Q3' 'Q4' 'Q5'; do
   for split in 'all' 'reference' 'peer'; do
     python -m sacrerouge correlate \
-      --metrics-jsonl-files datasets/duc-tac/duc2005/v1/task1.metrics.jsonl ${DIR}/output/scores.jsonl \
+      --metrics-jsonl-files datasets/duc-tac/duc2005/v1.0/task1.metrics.jsonl ${DIR}/output/scores.jsonl \
       --metrics linguistic_quality_${q} SumQE_${q} \
       --summarizer-type ${split} \
       --output-file ${DIR}/output/${split}/${q}.json
