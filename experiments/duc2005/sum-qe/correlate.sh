3e7556a4b81062b14acb70e89bf41bcf80f28f4e
Daniel Deutsch <danfdeutsch@gmail.com>
2020-04-12 13:44:48 -0400

    Typo

--- experiments/duc2005/sum-qe/correlate.sh
+++ experiments/duc2005/sum-qe/correlate.sh
@@ -3,10 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p ${DIR}/output
 
 for q in 'Q1' 'Q2' 'Q3' 'Q4' 'Q5'; do
-  for split in 'all' 'reference' 'peer'
-  python -m sacrerouge correlate \
-    --metrics-jsonl-files datasets/duc-tac/duc2005/v1/task1.metrics.jsonl ${DIR}/output/scores.jsonl \
-    --metrics linguistic_quality_${q} SumQE_${q} \
-    --summarizer-type ${split} \
-    --output-file ${DIR}/output/${split}/${q}.json
+  for split in 'all' 'reference' 'peer'; do
+    python -m sacrerouge correlate \
+      --metrics-jsonl-files datasets/duc-tac/duc2005/v1/task1.metrics.jsonl ${DIR}/output/scores.jsonl \
+      --metrics linguistic_quality_${q} SumQE_${q} \
+      --summarizer-type ${split} \
+      --output-file ${DIR}/output/${split}/${q}.json
+  done
 done
