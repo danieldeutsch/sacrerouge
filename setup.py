57624c1f65594e037978da1150fde61dfbf080bf
Daniel Deutsch <danfdeutsch@gmail.com>
2020-06-07 23:29:29 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='sacrerouge',
+    version='0.0.1',
+    author='Daniel Deutsch',
+    description='An open-source library for summarization evaluation metrics',
+    url='https://github.com/danieldeutsch/sacrerouge',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['sacrerouge=sacrerouge.__main__:main']},
+    python_requires='>=3.6'
+)
