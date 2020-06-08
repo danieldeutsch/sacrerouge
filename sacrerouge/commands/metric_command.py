d75054920b470a6e8d0840b82c91970f0e569592
Jonathan Peterson <JPeterson462@gmail.com>
2020-06-08 19:08:15 -0400

    Added constructor inspection to metric_command

--- sacrerouge/commands/metric_command.py
+++ sacrerouge/commands/metric_command.py
@@ -1,4 +1,5 @@
 import argparse
+import inspect
 from overrides import overrides
 
 from sacrerouge.commands import Subcommand
@@ -8,6 +9,8 @@ from sacrerouge.metrics import Metric
 def add_metric_subcommands(subparsers: argparse._SubParsersAction) -> None:
     """Adds a MetricSubcommand for every registered metric."""
     for name, metric in Metric._registry.items():
+        if not issubclass(metric, Metric):
+            continue
         command = MetricSubcommand(name, metric)
         command.add_subparser(subparsers)
 
@@ -26,6 +29,18 @@ class MetricSubcommand(Subcommand):
         # to self.parser. Somehow we might want to combine the default argument logic
         # below with the normal "evaluate" command
         # TODO
+
+        signature = inspect.signature(self.metric.__init__)
+        for param_name in signature.parameters:
+            name = signature.parameters[param_name].name
+            if name == 'self':
+                continue # Ignore self in constructor
+            default_value = signature.parameters[param_name].default
+            annotation = signature.parameters[param_name].annotation
+            attrs = dict()
+            attrs['default'] = default_value
+            attrs['type'] = annotation
+            self.parser.add_argument("--" + name, **attrs)
 
         self.parser.add_argument('macro_output_json')
         self.parser.add_argument('micro_output_jsonl')
