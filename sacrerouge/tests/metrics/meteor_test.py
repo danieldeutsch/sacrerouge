7f7236587c70f645eb476a604d0e7c04cbe184ff
Daniel Deutsch <danfdeutsch@gmail.com>
2020-06-11 22:36:48 -0400

    Converting test

--- sacrerouge/tests/metrics/meteor_test.py
+++ sacrerouge/tests/metrics/meteor_test.py
@@ -1,50 +1,34 @@
 import pytest
-import unittest
 
-from sacrerouge.common.testing import FIXTURES_ROOT, load_references, load_summaries
+from sacrerouge.common.testing import FIXTURES_ROOT
+from sacrerouge.common.testing.metric_test_cases import ReferenceBasedMetricTestCase
 from sacrerouge.io import JsonlReader
 from sacrerouge.metrics import Meteor
 
-_duc2004_file_path = 'datasets/duc-tac/duc2004/v1.0/task2.jsonl'
-_centroid_file_path = f'{FIXTURES_ROOT}/data/hong2014/centroid.jsonl'
 
-
-class TestMeteor(unittest.TestCase):
+class TestMeteor(ReferenceBasedMetricTestCase):
         def test_meteor(self):
-            """Verify METEOR runs"""
-            # These numbers aren't verified to be correct, but will catch if
-            # the code changes
-            meteor = Meteor()
-            duc2004 = load_references(_duc2004_file_path)
-            centroid = load_summaries(_centroid_file_path)
+            # This is a regression test, not necessarily a test for correctness
+            metric = Meteor()
+            expected_output = [
+                {'METEOR': 0.1669124164573825},
+                {'METEOR': 0.1948832291162732},
+                {'METEOR': 0.20611797558912198},
+                {'METEOR': 0.16514495147439157},
+                {'METEOR': 0.17681198359839032},
+                {'METEOR': 0.1711431947904745},
+                {'METEOR': 0.21743329361081287},
+                {'METEOR': 0.21450528119487908},
+                {'METEOR': 0.16550404185166837},
+                {'METEOR': 0.21164463762707278},
+                {'METEOR': 0.20412130512572657},
+                {'METEOR': 0.18243523574488876}
+            ]
+            super().assert_expected_output(metric, expected_output)
 
-            system_score, summary_scores = meteor.evaluate(centroid, duc2004)
-            assert system_score == pytest.approx({'METEOR': 0.16913291651219267}, abs=1e-4)
-            assert summary_scores[:5] == pytest.approx([
-                {'METEOR': 0.19100187219134385},
-                {'METEOR': 0.155452410194115},
-                {'METEOR': 0.1840667990852698},
-                {'METEOR': 0.18111121583550865},
-                {'METEOR': 0.13502476502555888}
-            ], abs=1e-5)
-
-        def test_score_multi_all_order(self):
-            """Tests to ensure the scoring returns the same results, no matter the order."""
-            meteor = Meteor()
-            duc2004 = load_references(_duc2004_file_path)
-            centroid1 = load_summaries(_centroid_file_path)
-            centroid2 = list(reversed(centroid1))  # Just create a second fake dataset
-
-            summaries_list = list(zip(*[centroid1, centroid2]))
-            metrics_lists1 = meteor.score_multi_all(summaries_list, duc2004)
-            metrics_lists1 = list(zip(*metrics_lists1))
-
-            summaries_list = list(zip(*[centroid2, centroid1]))
-            metrics_lists2 = meteor.score_multi_all(summaries_list, duc2004)
-            metrics_lists2 = list(zip(*metrics_lists2))
-
-            metrics_lists2 = list(reversed(metrics_lists2))
-            assert metrics_lists1 == metrics_lists2
+        def test_bewte_order_invariant(self):
+            metric = Meteor()
+            self.assert_order_invariant(metric)
 
         def test_chen2018(self):
             """
