639b8718285c7aea1f965733d71a921f7fd90160
Daniel Deutsch <danfdeutsch@gmail.com>
2020-06-11 23:33:51 -0400

    Refactoring to be more generic

--- sacrerouge/common/testing/metric_test_cases.py
+++ sacrerouge/common/testing/metric_test_cases.py
@@ -7,7 +7,7 @@ from sacrerouge.io import JsonlReader
 from sacrerouge.metrics import Metric
 
 
-class ReferenceBasedMetricTestCase(unittest.TestCase):
+class MetricTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
@@ -21,15 +21,15 @@ class ReferenceBasedMetricTestCase(unittest.TestCase):
                     references.append(reference['text'])
                 cls.references_list.append(references)
 
-    def assert_expected_output(self, metric: Metric, expected_output: List[MetricsDict]):
+    def _assert_expected_output(self, metric: Metric, expected_output: List[MetricsDict], *args):
         """Ensures that the output from `score_all` is equal to the `expected_output`."""
         assert len(self.summaries) == len(expected_output)
-        actual_output = metric.score_all(self.summaries, self.references_list)
+        actual_output = metric.score_all(self.summaries, *args)
         print(actual_output)
         for i, (expected, actual) in enumerate(zip(expected_output, actual_output)):
             assert actual.approx_equal(MetricsDict(expected), abs=1e-4), f'Instance {i} not equal. Expected {expected}, actual {actual}'
 
-    def assert_order_invariant(self, metric: Metric):
+    def _assert_order_invariant(self, metric: Metric, *args):
         """Ensures that the output from `score_multi_all` returns the same results, no matter the order."""
         # Reverse the summaries to create a new fake set of summaries that will be grouped together
         # with the same references. It does not matter that they're not the right references since we are
@@ -37,12 +37,28 @@ class ReferenceBasedMetricTestCase(unittest.TestCase):
         faked = list(reversed(self.summaries))
 
         summaries_list = list(zip(*[self.summaries, faked]))
-        metrics_lists1 = metric.score_multi_all(summaries_list, self.references_list)
+        metrics_lists1 = metric.score_multi_all(summaries_list, *args)
         metrics_lists1 = list(zip(*metrics_lists1))
 
         summaries_list = list(zip(*[faked, self.summaries]))
-        metrics_lists2 = metric.score_multi_all(summaries_list, self.references_list)
+        metrics_lists2 = metric.score_multi_all(summaries_list, *args)
         metrics_lists2 = list(zip(*metrics_lists2))
 
         metrics_lists2 = list(reversed(metrics_lists2))
         assert metrics_lists1 == metrics_lists2
+
+
+class ReferenceBasedMetricTestCase(MetricTestCase):
+    def assert_expected_output(self, metric: Metric, expected_output: List[MetricsDict]):
+        self._assert_expected_output(metric, expected_output, self.references_list)
+
+    def assert_order_invariant(self, metric: Metric):
+        self._assert_order_invariant(metric, self.references_list)
+
+
+class ReferencelessMetricTestCase(MetricTestCase):
+    def assert_expected_output(self, metric: Metric, expected_output: List[MetricsDict]):
+        self._assert_expected_output(metric, expected_output)
+
+    def assert_order_invariant(self, metric: Metric):
+        self._assert_order_invariant(metric)
