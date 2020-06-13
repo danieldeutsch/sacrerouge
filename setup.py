7c41c5fad373a46b94bb61c89e2ff258040f91fa
Daniel Deutsch <danfdeutsch@gmail.com>
2020-06-13 17:46:15 -0400

    Adding install requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ import setuptools
 
 setuptools.setup(
     name='sacrerouge',
-    version='0.0.2',
+    version='0.0.3',
     author='Daniel Deutsch',
     description='An open-source library for summarization evaluation metrics',
     url='https://github.com/danieldeutsch/sacrerouge',
     packages=setuptools.find_packages(),
     entry_points={'console_scripts': ['sacrerouge=sacrerouge.__main__:main']},
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    install_requires=[
+        'googledrivedownloader',
+        'jsons',
+        'lxml',
+        'nltk',
+        'numpy',
+        'overrides',
+        'pytest',
+        'requests',
+        'scipy'
+    ]
 )
