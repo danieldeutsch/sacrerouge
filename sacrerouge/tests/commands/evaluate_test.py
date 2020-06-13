ed388f0479612cf3f17605ca161a5967e526fc21
Daniel Deutsch <danfdeutsch@gmail.com>
2020-06-13 13:56:24 -0400

    Removing DUC/TAC dependence

--- sacrerouge/tests/commands/evaluate_test.py
+++ sacrerouge/tests/commands/evaluate_test.py
@@ -1,38 +1,33 @@
 import json
-import os
-import pytest
 import unittest
 from subprocess import PIPE, Popen
 from typing import Dict, List
 
 from sacrerouge.common import TemporaryDirectory
-from sacrerouge.common.testing import FIXTURES_ROOT
+from sacrerouge.common.testing import FIXTURES_ROOT, MULTILING_SUMMARIES
 from sacrerouge.data import Metrics
 from sacrerouge.io import JsonlReader
 
-_summaries_file_path = 'datasets/duc-tac/tac2008/v1.0/task1.A.summaries.jsonl'
 _config_file_path = f'{FIXTURES_ROOT}/configs/evaluate.json'
 
 
 class TestEvaluate(unittest.TestCase):
     def _check_macro(self, macro: Dict) -> None:
-        # I did not check these for correctness (other than the correctness of PythonRouge
-        # itself), but this should catch if anything changes
         assert macro['metrics'] == {
             'python-rouge-1': {
-                'precision': 34.728410791962965,
-                'recall': 34.36775110547158,
-                'f1': 34.488188700511316
+                'precision': 47.288299019868255,
+                'recall': 46.36412967789611,
+                'f1': 46.734012277932806
             },
             'python-rouge-2': {
-                'precision': 8.185740108131075,
-                'recall': 8.132737972371066,
-                'f1': 8.146449089653041
+                'precision': 16.625951207280107,
+                'recall': 16.30045341659763,
+                'f1': 16.430312150593775
             }
         }
 
     def _check_micro_list(self, micro_list: List[Metrics]) -> None:
-        instances = JsonlReader(_summaries_file_path).read()
+        instances = JsonlReader(MULTILING_SUMMARIES).read()
 
         assert len(micro_list) == len(instances)
         for micro, instance in zip(micro_list, instances):
@@ -40,35 +35,33 @@ class TestEvaluate(unittest.TestCase):
             assert micro.summarizer_id == instance['summarizer_id']
             assert micro.summarizer_type == instance['summarizer_type']
 
-        # Test a couple of cases. Again, I did not manually test these, but they
-        # will catch if anything changes
-        assert micro_list[1].metrics == {
+        assert micro_list[0].metrics == {
             'python-rouge-1': {
-                'precision': 30.412371134020617,
-                'recall': 29.72292191435768,
-                'f1': 30.06369426751592
+                'precision': 41.699867197875164,
+                'recall': 40.516129032258064,
+                'f1': 41.09947643979057
             },
             'python-rouge-2': {
-                'precision': 2.604166666666667,
-                'recall': 2.5445292620865136,
-                'f1': 2.5740025740025736
+                'precision': 10.533333333333333,
+                'recall': 10.233160621761659,
+                'f1': 10.38107752956636
             }
         }
-        assert micro_list[2848].metrics == {
+        assert micro_list[11].metrics == {
             'python-rouge-1': {
-                'precision': 29.207920792079207,
-                'recall': 28.780487804878046,
-                'f1': 28.99262899262899
+                'precision': 45.34412955465587,
+                'recall': 44.71057884231537,
+                'f1': 45.0251256281407
             },
             'python-rouge-2': {
-                'precision': 8.5,
-                'recall': 8.374384236453201,
-                'f1': 8.436724565756823
+                'precision': 15.24390243902439,
+                'recall': 15.030060120240481,
+                'f1': 15.136226034308779
             }
         }
 
-    @pytest.mark.skipif(not os.path.exists(_summaries_file_path), reason='TAC 2008 summaries file does not exist')
     def test_evaluate(self):
+        # This is a regresion test and does not ensure correctness
         with TemporaryDirectory() as temp_dir:
             macro_file = f'{temp_dir}/macro.json'
             micro_file = f'{temp_dir}/micro.jsonl'
@@ -81,7 +74,7 @@ class TestEvaluate(unittest.TestCase):
             ]
 
             process = Popen(command, stdout=PIPE, stderr=PIPE)
-            stdout, stderr = process.communicate()
+            process.communicate()
 
             macro_metrics = json.load(open(macro_file, 'r'))
             micro_metrics_list = JsonlReader(micro_file, Metrics).read()
