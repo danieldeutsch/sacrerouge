348f68bdabe736d992e7134f51d2ca9e47c4f056
Daniel Deutsch <danfdeutsch@gmail.com>
2020-08-05 22:24:17 -0400

    Generating reference annotations

--- sacrerouge/data/dataset_readers/pyramid_based.py
+++ sacrerouge/data/dataset_readers/pyramid_based.py
@@ -11,6 +11,16 @@ logger = logging.getLogger(__name__)
 
 @DatasetReader.register('pyramid-based')
 class PyramidBasedDatasetReader(DatasetReader):
+    def __init__(self, include_reference_annotations: bool = True) -> None:
+        """
+        Args:
+            include_reference_annotations: Indicates if pyramid annotations should be created
+                for all of the summaries in the pyramid. This will be done for all of the instance_ids
+                which are observed in the annotations file.
+        """
+        super().__init__()
+        self.include_reference_annotations = include_reference_annotations
+
     def read(self,
              pyramid_jsonl: str,
              annotation_jsonl: str) -> List[EvalInstance]:
@@ -23,6 +33,7 @@ class PyramidBasedDatasetReader(DatasetReader):
 
         logger.info(f'Loading Pyramid annotations from {annotation_jsonl}')
         instances = []
+        instance_ids = set()
         with JsonlReader(annotation_jsonl, PyramidAnnotation) as f:
             for annotation in f:
                 fields = Fields({
@@ -38,5 +49,31 @@ class PyramidBasedDatasetReader(DatasetReader):
                 )
                 instances.append(instance)
 
+                instance_ids.add(annotation.instance_id)
+
             logger.info(f'Loaded {len(instances)} Pyramid annotations')
-            return instances
+
+        if self.include_reference_annotations:
+            logger.info(f'Generating Pyramid annotations for the reference summaries')
+            reference_instances = []
+            for instance_id in instance_ids:
+                pyramid = pyramids[instance_id]
+                for i in range(len(pyramid.summarizer_ids)):
+                    annotation = pyramid.get_annotation(i)
+                    reduced_pyramid = pyramid.remove_summary(i)
+                    fields = Fields({
+                        'annotation': PyramidAnnotationField(annotation),
+                        'pyramid': PyramidField(reduced_pyramid)
+                    })
+                    instance = EvalInstance(
+                        annotation.instance_id,
+                        annotation.summarizer_id,
+                        annotation.summarizer_type,
+                        fields
+                    )
+                    reference_instances.append(instance)
+            logger.info(f'Generated {len(reference_instances)} reference summary annotations')
+            instances.extend(reference_instances)
+
+        logger.info(f'Loaded a total of {len(instances)} instances')
+        return instances
