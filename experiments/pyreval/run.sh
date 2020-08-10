801c19b84ed67136e34e3d77b31c604fbc99c4c0
Daniel Deutsch <danfdeutsch@gmail.com>
2020-08-10 18:49:14 -0400

    Uncommenting experiment

--- experiments/pyreval/run.sh
+++ experiments/pyreval/run.sh
@@ -1,30 +1,30 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-#for dataset in tac2010 tac2011; do
-#  python -m sacrerouge pyreval score \
-#    ${DIR}/output/${dataset}/scores.jsonl \
-#    --input-files datasets/duc-tac/${dataset}/v1.0/task1.A.summaries.jsonl \
-#    --dataset-reader reference-based \
-#    --verbose true \
-#    --environment_name /shared/ddeutsch/envs/pyreval \
-#    --log-file ${DIR}/output/${dataset}/log.log
-#
-#  for metric in raw quality coverage comprehensive; do
-#    python -m sacrerouge correlate \
-#      --metrics-jsonl-files datasets/duc-tac/${dataset}/v1.0/task1.A.metrics.jsonl ${DIR}/output/${dataset}/scores.jsonl \
-#      --metrics overall_responsiveness pyreval_${metric} \
-#      --summarizer-type peer \
-#      --output-file ${DIR}/output/${dataset}/correlations/${metric}-peer.json \
-#      --log-file ${DIR}/output/${dataset}/correlations/log.log
-#
-#    python -m sacrerouge correlate \
-#      --metrics-jsonl-files datasets/duc-tac/${dataset}/v1.0/task1.A.metrics.jsonl ${DIR}/output/${dataset}/scores.jsonl \
-#      --metrics overall_responsiveness pyreval_jk_${metric} \
-#      --summarizer-type all \
-#      --output-file ${DIR}/output/${dataset}/correlations/${metric}-all.json \
-#      --log-file ${DIR}/output/${dataset}/correlations/log.log
-#  done
-#done
+for dataset in tac2008 tac2009 tac2010 tac2011; do
+  python -m sacrerouge pyreval score \
+    ${DIR}/output/${dataset}/scores.jsonl \
+    --input-files datasets/duc-tac/${dataset}/v1.0/task1.A.summaries.jsonl \
+    --dataset-reader reference-based \
+    --verbose true \
+    --environment_name /shared/ddeutsch/envs/pyreval \
+    --log-file ${DIR}/output/${dataset}/log.log
+
+  for metric in raw quality coverage comprehensive; do
+    python -m sacrerouge correlate \
+      --metrics-jsonl-files datasets/duc-tac/${dataset}/v1.0/task1.A.metrics.jsonl ${DIR}/output/${dataset}/scores.jsonl \
+      --metrics overall_responsiveness pyreval_${metric} \
+      --summarizer-type peer \
+      --output-file ${DIR}/output/${dataset}/correlations/${metric}-peer.json \
+      --log-file ${DIR}/output/${dataset}/correlations/log.log
+
+    python -m sacrerouge correlate \
+      --metrics-jsonl-files datasets/duc-tac/${dataset}/v1.0/task1.A.metrics.jsonl ${DIR}/output/${dataset}/scores.jsonl \
+      --metrics overall_responsiveness pyreval_jk_${metric} \
+      --summarizer-type all \
+      --output-file ${DIR}/output/${dataset}/correlations/${metric}-all.json \
+      --log-file ${DIR}/output/${dataset}/correlations/log.log
+  done
+done
 
 for summarizer_type in 'all' 'peer'; do
   for level in 'summary_level' 'system_level'; do
