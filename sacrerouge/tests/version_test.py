f81a93aee3d663937dd704a817883d2ccb944fe5
Daniel Deutsch <danfdeutsch@gmail.com>
2020-10-01 23:15:57 -0400

    Removing comment

--- sacrerouge/tests/version_test.py
+++ sacrerouge/tests/version_test.py
@@ -5,5 +5,4 @@ import sacrerouge
 
 class TestVersion(unittest.TestCase):
     def test_version_exists(self):
-        # Test to make sure the variable exists and is parsable
         assert sacrerouge.__version__
