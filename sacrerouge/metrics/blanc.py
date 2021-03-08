f9310d899c17d1816f8cbe4d3215c1b8181ddeae
Daniel Deutsch <danieldeutsch@users.noreply.github.com>
2021-03-08 10:12:29 -0500

    Fixing default model type

--- sacrerouge/metrics/blanc.py
+++ sacrerouge/metrics/blanc.py
@@ -29,7 +29,7 @@ else:
     @Metric.register('blanc')
     class Blanc(DocumentBasedMetric):
         def __init__(self,
-                     blanc_type: str = 'blanc_tune',
+                     blanc_type: str = 'blanc_help',
                      device: str = 'cuda',
                      gap: int = 2,
                      inference_batch_size: int = 24,
